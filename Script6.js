5d2470aaca9de1e07804366432d4df7d3a2d1b66
unknown <anahitxachatryan11@gmail.com>
2019-02-06 23:24:59 +0400

    Find the sign of the product of three numbers without multiplication operator

---
+++ Script6.js
@@ -0,0 +1,13 @@
+//9. Find the sign of product of three numbers without multiplication operator.Display the specified sign.
+
+var x = +prompt("Please enter first number");
+var y = +prompt("Please enter second number");
+var z = +prompt("Please enter second number");
+
+if ((x > 0 && y > 0 && z > 0) || (x < 0 && y < 0 && z > 0) || (x < 0 && y > 0 && z < 0) || (x > 0 && y < 0 && z < 0)) {
+    console.log("The sign is positive");
+}
+else {
+
+    console.log("The sign is positive");
+}
